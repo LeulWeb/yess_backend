e3feeb9ee7d71b0aa7d781a8bc7d2eed0fcc2420
bishu <leulwebsdev@gmail.com>
2024-02-15 00:55:35 +0300

    update the yout api order

--- app/Http/Controllers/Api/YouthController.php
+++ app/Http/Controllers/Api/YouthController.php
@@ -13,9 +13,13 @@ class YouthController extends Controller
     {
         $searchTerm = $request->query('query');
 
-        return Youth::select('id', 'video_link', 'user_id', 'is_published',"achievment")->with(['user:id,name,phone,email,profile_picture,story'])->whereHas('user', function ($query) use ($searchTerm) {
-            $query->where('name', 'like', '%' . $searchTerm . '%');
-        })->get();
+        return Youth::select('id', 'video_link', 'user_id', 'is_published', "achievment")
+            ->with(['user:id,name,phone,email,profile_picture,story'])
+            ->whereHas('user', function ($query) use ($searchTerm) {
+                $query->where('name', 'like', '%' . $searchTerm . '%');
+            })
+            ->latest('created_at') // Order by the 'created_at' column of the 'youths' table
+            ->get();
     }
 
     public function show(string $id)
