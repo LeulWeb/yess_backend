c3f92e6a829d4163c80c7b31921b878369b00a76
bishu <leulwebsdev@gmail.com>
2024-02-16 11:29:44 +0300

    commit for final pull

--- resources/views/livewire/mobile-app/version.blade.php
+++ resources/views/livewire/mobile-app/version.blade.php
@@ -8,9 +8,9 @@
 
         <p class="mb-3 text-lg text-red-500 md:text-xl dark:text-red-400">Warning</p>
         <p class="text-red-500 dark:text-red-400">Track work across the enterprise Attention: Application Update
-            Required\n\nDear Admin,\n\nWe have detected that the current version of the application is outdated. It is
+            Required Dear Admin,We have detected that the current version of the application is outdated. It is
             essential to update to the latest version to ensure optimal performance, security, and access to new
-            features.\n\nPlease take immediate action to update the application to the latest version to avoid any
+            features.Please take immediate action to update the application to the latest version to avoid any
             potential issues and to benefit from the latest enhancements. <br>Thank you for your attention
         </p>
 
@@ -65,13 +65,14 @@
                                 d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
                         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to
-                            change the app version from {{$version->version}} to {{$currentVersion}} this will prompt user for upate?</h3>
+                            change the app version from {{ $version->version }} to {{ $currentVersion }} this will
+                            prompt user for upate?</h3>
 
 
                         <button data-modal-hide="popup-modal" type="submit"
                             class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Update
                             Version</button>
-                        <button  type="button"
+                        <button type="button"
                             class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No,
                             cancel</button>
                     </div>
